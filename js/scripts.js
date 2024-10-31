8801c01ac32154e885a18af5ffe7e0ff9a94da7f
foferys <foferys@gmail.com>
2024-10-31 11:08:47 +0100

    sistemazioni css varie

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,10 @@
 // Use this file to add JavaScript to your project
 
 const logo = document.querySelector("#mainLogo")
+const navLink = document.querySelectorAll(".nav-link")
+const menu = document.querySelector(".offcanvas")
+const backBlack = document.querySelector(".offcanvas-backdrop")
+const contattiBut = document.querySelector("#contattiBut")
 
 window.addEventListener("scroll", function() {
 
@@ -20,3 +24,20 @@ window.addEventListener("scroll", function() {
         logo.src="./assets/logoMex-exwhite.png";
     }
 })
+
+function closeMenu() {
+    //prendo il bottone dopo aver cliccato perché altrimenti non lo vede visto che all'inizio l'elemento non è presente
+    const backBlack = document.querySelector(".offcanvas-backdrop")
+    menu.classList.remove("show");
+
+    navLink.forEach(element => {
+        element.classList.remove("active");
+    });
+    contattiBut.classList.add("active");
+
+
+    if(backBlack) {
+
+        backBlack.style.display = "none"
+    }
+}
